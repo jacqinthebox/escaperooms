076adce99fda9c276b6eb799472bc482192547e2
Jacqueline <jacq@in-the-box.nl>
2023-01-08 17:09:38 +0100

    add keda

--- src/roomservice/app/main.py
+++ src/roomservice/app/main.py
@@ -3,11 +3,30 @@ from dapr.clients import DaprClient
 import json
 import logging
 from flask_cors import CORS
+import os
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
 
+sb_connstring = os.getenv("SB_CONNSTRING")  # localhost or sqledge (container name)
+sb_queuename = os.getenv("SB_QUEUENAME")  # whatever db
 
 app = Flask(__name__)
 CORS(app)
 logging.basicConfig(level=logging.INFO)
+
+
+def send_single_message(sender):
+    message = ServiceBusMessage("Single Picture")
+    sender.send_messages(message)
+    print("Sent a single picture")
+
+
+def send_a_list_of_messages(sender):
+    messages = [ServiceBusMessage("List of pictures") for _ in range(5)]
+    sender.send_messages(messages)
+    print("Sent a list of 5 pictures")
+
+
+servicebus_client = ServiceBusClient.from_connection_string(conn_str=sb_connstring, logging_enable=True)
 
 
 @app.route('/room/leave', methods=['POST'])
@@ -24,6 +43,12 @@ def leave():
             data=json.dumps(team),
             data_content_type='application/json',
         )
+
+    with servicebus_client:
+        sender = servicebus_client.get_queue_sender(queue_name=sb_queuename)
+        with sender:
+            send_single_message(sender)
+            send_a_list_of_messages(sender)
 
     logging.info('Published data: ' + json.dumps(team))
 
